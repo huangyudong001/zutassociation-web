193a180cb28a3705281e1122f934ebdfe1d9df8f
黄玉东 <1410995914@qq.com>
2021-02-24 11:17:44 +0800

    bug

--- zutassociation-vue/src/main.js
+++ zutassociation-vue/src/main.js
@@ -50,15 +50,6 @@ Vue.component('RightToolbar', RightToolbar)
 
 Vue.use(permission)
 
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online! ! !
- */
-
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
 })
